921e33241d1cf02339dd06600f6f329a08714e32
Kevin <Klee@stud.hs-heilbronn.de>
2016-06-05 13:46:27 +0200

    no nullpointer anymore

--- src/main/java/controller/NextLecture.java
+++ src/main/java/controller/NextLecture.java
@@ -1,10 +1,12 @@
 package controller;
 
+
 import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import model.Course;
 import model.User;
+
 
 import java.time.LocalDateTime;
 
@@ -16,38 +18,35 @@ public class NextLecture extends Modul {
 
     private LocalDateTime nextLecture;
     private Course c;
+    private Label next;
 
     public NextLecture(User user) {
         super(user);
-        nextLecture = null;
-        setupLayout(user);
+
+        if(!user.getCourses().isEmpty()) {
+            nextLecture = user.getCourses().get(0).getDates().get(0);
+            setupLayout(user);
+        }
+        else {
+            next = new Label("Benutzer hat keine Vorlesungen", ContentMode.HTML);
+        }
     }
 
     private void setupLayout(User user)
     {
         user.getCourses().forEach(course -> {
 
-            if(course.getDates().get(0).isBefore(nextLecture) || nextLecture == null )
+            if(course.getDates().get(0).isBefore(nextLecture))
             {
                 nextLecture = course.getDates().get(0);
                 c = course;
             }
-
         });
-
-        Label next;
-
-        if(nextLecture == null)
-        {
-            next = new Label("Benutzer hat keine Vorlesungen", ContentMode.HTML);
-        }
-        else
-        {
-            next = new Label("<u>Nächste Vorlesung</u>" +
+        next = new Label("<u>Nächste Vorlesung</u>" +
                             "<br>" + c.getName() +
                             "<br>Zeit: " + c.getDates().get(0).toString() +
                              "<br>Raum: " + c.getRoom(), ContentMode.HTML);
-        }
+
 
         HorizontalLayout horilayout = new HorizontalLayout();
         horilayout.addComponent(next);
